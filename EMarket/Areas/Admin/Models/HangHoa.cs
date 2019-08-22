4d1ef0358c36cebb5c6747798e782b63570be226
Nguyen Hoang Anh Tu <nguyenhoanganhtu2406@gmail.com>
2019-08-22 20:09:39 +0700

    Fix bug "Self referencing loop" JSON

--- EMarket/Areas/Admin/Models/HangHoa.cs
+++ EMarket/Areas/Admin/Models/HangHoa.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace EMarket.Areas.Admin.Models
 {
+    [JsonObject(IsReference = true)]
     public partial class HangHoa
     {
         public HangHoa()
